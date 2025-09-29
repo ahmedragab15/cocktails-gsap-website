1e5ba27680e319a5e4122f3a9b86f6cc0d3fafb2
ahmedragab15 <ragabahmed154@gmail.com>
2025-09-29 21:20:38 +0300

    Implement Hero Section

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./sections/Navbar";
+import Hero from "./sections/Hero";
 
 gsap.registerPlugin(useGSAP, ScrollTrigger, SplitText);
 const App = () => {
   return (
     <main>
       <Navbar />
+      <Hero />
+      <div className="h-dvh bg-black"></div>
     </main>
   );
 };
